7a0cf74aa050b9c6006d963193a5476b002e9fd0
Jakhetia, Nidhi <njakheti@itron.com>
2020-07-10 17:45:58 +0530

    Added the Test Cases

--- PromotionEngine.UnitTests/PromotionEngineTests.cs
+++ PromotionEngine.UnitTests/PromotionEngineTests.cs
@@ -1,13 +1,56 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PromotionEngine.Models;
+using System.Collections.Generic;
 
 namespace PromotionEngine.UnitTests
 {
     [TestClass]
     public class PromotionEngineTests
     {
-        [TestMethod]
-        public void TestMethod1()
+        public OrderProcessor orderProcessor;
+
+        [TestInitialize]
+        public void TestInitialize()
         {
+            orderProcessor = new OrderProcessor();
+        }
+
+        [TestMethod]
+        public void PromotionEngineTests_Scenario1()
+        {
+            decimal expectedValue = 100;
+
+            Order order = new Order(1, new List<Product>() { new Product("A"), new Product("B"), new Product("C") });
+
+            decimal totalOrderValue = orderProcessor.GetTotalOrderValue(order);
+
+            Assert.AreEqual(expectedValue, totalOrderValue);
+        }
+
+        [TestMethod]
+        public void PromotionEngineTests_Scenario2()
+        {
+            decimal expectedValue = 370;
+
+            Order order = new Order(1, new List<Product>() { new Product("A"), new Product("A"), new Product("A"), new Product("A"), new Product("A"), new Product("B"), new Product("B"),
+            new Product("B"),new Product("B"),new Product("B"),new Product("C")});
+
+            decimal totalOrderValue = orderProcessor.GetTotalOrderValue(order);
+
+            Assert.AreEqual(expectedValue, totalOrderValue);
+        }
+
+        [TestMethod]
+        public void PromotionEngineTests_Scenario3()
+        {
+            decimal expectedValue = 280;
+
+            Order order = new Order(1, new List<Product>() { new Product("A"), new Product("A"), new Product("A"), new Product("B"), new Product("B"),
+            new Product("B"),new Product("B"),new Product("B"),new Product("C"), new Product("D")});
+
+            decimal totalOrderValue = orderProcessor.GetTotalOrderValue(order);
+
+            Assert.AreEqual(expectedValue, totalOrderValue);
         }
     }
 }
