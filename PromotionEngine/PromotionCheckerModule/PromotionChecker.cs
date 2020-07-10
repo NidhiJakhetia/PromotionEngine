09de66368ee4bea4d446bd23013f9a2af29a3b96
Jakhetia, Nidhi <njakheti@itron.com>
2020-07-10 17:36:43 +0530

    Moved the two methods from promotionchecker class to helper class

--- PromotionEngine/PromotionCheckerModule/PromotionChecker.cs
+++ PromotionEngine/PromotionCheckerModule/PromotionChecker.cs
@@ -18,7 +18,7 @@ namespace PromotionEngine.PromotionCheckerModule
 
             if (count == 0)
             {
-                totalPrice = NoPromoApplied(order, promotion, totalPrice);
+                totalPrice = Helper.NoPromoApplied(order, promotion, totalPrice);
                 return totalPrice;
             }
 
@@ -33,118 +33,10 @@ namespace PromotionEngine.PromotionCheckerModule
 
             if (count != 0 && count < promotedProductscount)
             {
-                totalPrice = LeftSkuCalculation(order, promotion, totalPrice, count);
+                totalPrice = Helper.LeftSkuCalculation(order, promotion, totalPrice, count);
             }
 
             return totalPrice;
-        }
-
-        private static decimal LeftSkuCalculation(Order ord, Promotion prom, decimal d, int copp)
-        {
-            var sku = ord.Products
-                       .GroupBy(x => x.SkuId)
-                       .Where(grp => prom.ProductInfo.Any(y => grp.Key == y.Key))
-                       .Select(grp => grp.Key).FirstOrDefault();
-
-            if (sku == "A")
-            {
-                int priceofA = 50;
-
-                d = d + (copp * priceofA);
-            }
-            if (sku == "B")
-            {
-                int priceofB = 30;
-
-                d = d + (copp * priceofB);
-            }
-            if (sku == "C")
-            {
-                int priceofC = 20;
-
-                d = d + (copp * priceofC);
-            }
-            if (sku == "D")
-            {
-                int priceofD = 15;
-
-                d = d + (copp * priceofD);
-            }
-            return d;
-        }
-
-        private static decimal NoPromoApplied(Order ord, Promotion prom, decimal d)
-        {
-            var sku = ord.Products
-                       .GroupBy(x => x.SkuId)
-                       .Where(grp => prom.ProductInfo.Any(y => grp.Key == y.Key))
-                       .Select(grp => grp.Key).FirstOrDefault();
-
-            if (sku == "A")
-            {
-                int counterofA = 0;
-                int priceofA = 50;
-                var listOfProducts = ord.Products;
-                foreach (Product pr in listOfProducts)
-                {
-                    if (pr.SkuId == "A" || pr.SkuId == "a")
-                    {
-                        counterofA = counterofA + 1;
-                    }
-                }
-
-                d = counterofA * priceofA;
-            }
-
-            if (sku == "B")
-            {
-                int counterofB = 0;
-                int priceofB = 30;
-                var listOfProducts = ord.Products;
-                foreach (Product pr in listOfProducts)
-                {
-                    if (pr.SkuId == "B" || pr.SkuId == "b")
-                    {
-                        counterofB = counterofB + 1;
-                    }
-                }
-
-                d = counterofB * priceofB;
-            }
-
-            if (sku == "C")
-            {
-                int counterofC = 0;
-                int priceofC = 20;
-                var listOfProducts = ord.Products;
-                foreach (Product pr in listOfProducts)
-                {
-                    if (pr.SkuId == "C" || pr.SkuId == "c")
-                    {
-                        counterofC = counterofC + 1;
-                    }
-                }
-
-                d = counterofC * priceofC;
-            }
-
-            if (sku == "D")
-            {
-                int counterofD = 0;
-                int priceofD = 15;
-                var listOfProducts = ord.Products;
-                foreach (Product pr in listOfProducts)
-                {
-                    if (pr.SkuId == "D" || pr.SkuId == "d")
-                    {
-                        counterofD = counterofD + 1;
-                    }
-                }
-
-                d = counterofD * priceofD;
-            }
-
-            return d;
-        }
+        }        
     }
 }
