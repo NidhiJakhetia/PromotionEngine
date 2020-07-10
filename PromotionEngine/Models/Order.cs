902a9a2fcd72547b40f93ce9ece8fe07ed81bb1f
Jakhetia, Nidhi <njakheti@itron.com>
2020-07-10 16:32:58 +0530

    Added Models in Promotion Engine

---
+++ PromotionEngine/Models/Order.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace PromotionEngine.Models
+{
+    public class Order
+    {
+        public int OrderId { get; set; }
+        public List<Product> Products { get; set; }
+
+        public Order(int _orderId, List<Product> _products)
+        {
+            this.OrderId = _orderId;
+            this.Products = _products;
+        }
+    }
+}
