2559f9641fa5c7a8235bac19cc3865336095b839
Jakhetia, Nidhi <njakheti@itron.com>
2020-07-10 17:16:23 +0530

    Added Logic for Promotion Calculation

--- PromotionEngine/OrderProcessor.cs
+++ PromotionEngine/OrderProcessor.cs
@@ -9,7 +9,15 @@ namespace PromotionEngine
     {
         public decimal GetTotalOrderValue(Order order)
         {
-            
+            PromotionChecker promotionChecker = new PromotionChecker();
+
+            IList<Promotion> promotions = ActivePromotions.GetActivePromotions(); //will get the list of Active promotions
+
+            List<decimal> promoprices = promotions
+                    .Select(promo => promotionChecker.GetTotalPrice(order, promo))
+                    .ToList();
+            decimal promoprice = promoprices.Sum();
+            return promoprice;
         }
     }
 }
--- PromotionEngine/PromotionCheckerModule/PromotionChecker.cs
+++ PromotionEngine/PromotionCheckerModule/PromotionChecker.cs
@@ -7,7 +7,24 @@ namespace PromotionEngine.PromotionCheckerModule
     {
         public decimal GetTotalPrice(Order order, Promotion promotion)
         {
-            
+            decimal totalPrice = 0M;
+
+            //It will fetch the count of products eligible for promotion.
+            int count = order.Products
+                .GroupBy(x => x.SkuId)
+                .Where(grp => promotion.ProductInfo.Any(y => grp.Key == y.Key && grp.Count() >= y.Value))
+                .Select(grp => grp.Count())
+                .Sum();
+
+            //It will fetch the count of products in promotion
+            int promotedProductscount = promotion.ProductInfo.Sum(kvp => kvp.Value);
+
+            while (count >= promotedProductscount)
+            {
+                totalPrice += promotion.PromoPrice;
+                count -= promotedProductscount;
+            }
+            return totalPrice;
         }
     }
 }
