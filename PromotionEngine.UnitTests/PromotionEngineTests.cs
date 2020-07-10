d08fb9b92692403975cfc990be022d9a32c06fb5
Jakhetia, Nidhi <njakheti@itron.com>
2020-07-10 16:59:08 +0530

    Added a Unit Test Project

---
+++ PromotionEngine.UnitTests/PromotionEngineTests.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PromotionEngine.UnitTests
+{
+    [TestClass]
+    public class PromotionEngineTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
